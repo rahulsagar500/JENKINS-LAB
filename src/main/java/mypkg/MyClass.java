bcd2edb0592c92af73b2626140890b1dfe18aa0e
rahulsagar500 <rahulsagarpabba5@gmail.com>
2023-02-19 12:39:35 +0530

    Second commit

--- src/main/java/mypkg/MyClass.java
+++ src/main/java/mypkg/MyClass.java
@@ -4,6 +4,9 @@ public class MyClass {
 	public void show() {
 		System.out.println("Welcome to JENKINS!");
 	}
+	public void display() {
+		System.out.println("This is a display function.");
+	}
 
 	public static void main(String[] args) {
 		MyClass ob=new MyClass();
