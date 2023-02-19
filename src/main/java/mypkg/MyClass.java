49b708f05bf56be0a6cefb854efe27fa9ece31f5
rahulsagar500 <rahulsagarpabba5@gmail.com>
2023-02-19 12:00:15 +0530

    First commit

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,13 @@
+package mypkg;
+
+public class MyClass {
+	public void show() {
+		System.out.println("Welcome to JENKINS!");
+	}
+
+	public static void main(String[] args) {
+		MyClass ob=new MyClass();
+
+	}
+
+}
